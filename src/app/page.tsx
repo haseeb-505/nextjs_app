5a90b0a3c03b55586591d135d4c8fb39d4685c1b
haseeb-505 <hatahir87626@gmail.com>
2025-05-29 01:56:23 +0500

    navbar added, dark mode added in basic layout file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,9 @@
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-       <h1 className="text-3xl font-bold text-blue-500">Hello, Tailwind!</h1>
-        
-      </main>
-    </div>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-500">Hello, Tailwind!</h1>
+      
+    </main>
   );
 }
