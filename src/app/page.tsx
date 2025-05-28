87b6c668ebf134e53387aeec311d90757198a548
haseeb-505 <hatahir87626@gmail.com>
2025-05-29 02:42:01 +0500

    added the spotlight and moving border to explore button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import HeroSection from "@/components/HeroSection";
 
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] flex items-center justify-center">
-      <h1 className="text-3xl font-bold text-blue-500">Hello, Tailwind!</h1>
-      
+      {/* <h1 className="text-3xl font-bold text-blue-500">Hello, Tailwind!</h1> */}
+      <HeroSection />
     </main>
   );
 }
